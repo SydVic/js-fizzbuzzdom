8bfd765aa2b61c5de413f2d938e2f432c3d19783
SydVic <fabrizio.bocca27@gmail.com>
2022-04-11 13:23:44 +0200

    aggiunto ciclo for da 1 a 100

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// scrivere programma che stampi in console i numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    console.log(i)
+}
+
+// per i multipli di 3 stampare FIZZ al posto del numero
+// per i multipli di 5 stampare BUZZ
+// per i multipli sia di 3 che di 5 stampare FIZZBUZZ
