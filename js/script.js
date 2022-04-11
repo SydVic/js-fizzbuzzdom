4bfc908bd2405d7958fdb8f4c49b2015b624295f
SydVic <fabrizio.bocca27@gmail.com>
2022-04-11 13:35:49 +0200

    aggiunto console lo Fizz Buzz e FizzBuzz con if

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 // scrivere programma che stampi in console i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    console.log(i)
-}
+    //console.log(i)
 
-// per i multipli di 3 stampare FIZZ al posto del numero
-// per i multipli di 5 stampare BUZZ
-// per i multipli sia di 3 che di 5 stampare FIZZBUZZ
+    let fizzBuzz;
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        // per i multipli sia di 3 che di 5 stampare FIZZBUZZ
+        fizzBuzz = "FizzBuzz";
+    } else if (i % 3 === 0) {
+        // per i multipli di 3 stampare FIZZ al posto del numero
+        fizzBuzz = "Fizz";
+    } else if (i % 5 === 0) {
+        // per i multipli di 5 stampare BUZZ
+        fizzBuzz = "Buzz";
+    } else {
+        fizzBuzz = i;
+    }
+    console.log(fizzBuzz)
+};
+
