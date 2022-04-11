0bc8db748d12bda1f84e6bcde10574296012e3ff
SydVic <fabrizio.bocca27@gmail.com>
2022-04-11 14:12:32 +0200

    corretta indentazione

--- js/script.js
+++ js/script.js
@@ -2,19 +2,19 @@
 for (let i = 1; i <= 100; i++) {
 //console.log(i)
 
-let fizzBuzz;
+    let fizzBuzz;
 
-if (i % 3 === 0 && i % 5 === 0) {
-    // per i multipli sia di 3 che di 5 stampare FIZZBUZZ
-    fizzBuzz = "FizzBuzz";
-} else if (i % 3 === 0) {
-    // per i multipli di 3 stampare FIZZ al posto del numero
-    fizzBuzz = "Fizz";
-} else if (i % 5 === 0) {
-    // per i multipli di 5 stampare BUZZ
-    fizzBuzz = "Buzz";
-} else {
-    fizzBuzz = i;
-}
-console.log(fizzBuzz);
+    if (i % 3 === 0 && i % 5 === 0) {
+        // per i multipli sia di 3 che di 5 stampare FIZZBUZZ
+        fizzBuzz = "FizzBuzz";
+    } else if (i % 3 === 0) {
+        // per i multipli di 3 stampare FIZZ al posto del numero
+        fizzBuzz = "Fizz";
+    } else if (i % 5 === 0) {
+        // per i multipli di 5 stampare BUZZ
+        fizzBuzz = "Buzz";
+    } else {
+        fizzBuzz = i;
+    }
+    console.log(fizzBuzz);
 };
