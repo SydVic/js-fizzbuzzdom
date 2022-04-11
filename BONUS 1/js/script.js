7ff69fd11ce95d4181569fd21c799b27a6c6f2b6
SydVic <fabrizio.bocca27@gmail.com>
2022-04-11 14:36:55 +0200

    aggiunto inneHTML per creare box

--- BONUS 1/js/script.js
+++ BONUS 1/js/script.js
@@ -1,3 +1,6 @@
+const rowBox = document.querySelector(".row");
+console.log(rowBox)
+
 // scrivere programma che stampi in console i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
 //console.log(i)
@@ -17,4 +20,8 @@ for (let i = 1; i <= 100; i++) {
         fizzBuzz = i;
     }
     console.log(fizzBuzz);
+
+    // creo i box per ogni valore del ciclo for
+    rowBox.innerHTML += `<div class="box">${fizzBuzz}</div>`;
+
 };
